8dad5c2b1070490b38045e9fe3783a85852db767
Cicero <chgs@icomp.ufam.edu.br>
2024-05-16 22:27:19 -0400

    Adicionando código do smartlamp

---
+++ smartlamp/smartlamp.ino
@@ -0,0 +1,118 @@
+// Defina os pinos de LED e LDR
+// Defina uma variável com valor máximo do LDR (4000)
+// Defina uma variável para guardar o valor atual do LED (10)
+
+
+/*
+ * https://gist.github.com/aferreira44/6bbe8fab792e24afc6c1389da714aa99
+ * https://embarcados.com.br/como-programar-o-esp32-na-arduino-ide/
+ * https://kb.deec.uc.pt/books/deec/page/programa-led-a-piscar
+ * https://portal.vidadesilicio.com.br/led-com-esp32-curso-esp32-basico/
+ * https://portal.vidadesilicio.com.br/como-utilizar-o-led-rgb-com-arduino/
+ * https://www.paulotrentin.com.br/programacao/dicas/lendo-uma-string-com-arduino-via-serial/
+ * https://www.youtube.com/watch?v=GDxwDsphyuk&ab_channel=BrincandocomIdeias
+ * https://blogmasterwalkershop.com.br/arduino/arduino-exibindo-e-lendo-dados-da-serial
+ * https://forum.arduino.cc/t/test-if-a-string-contains-a-string/478927/4
+ * https://www.blogdarobotica.com/2022/06/30/controlando-o-brilho-do-led-usando-potenciometro-e-arduino/
+ * https://www.tutorialspoint.com/find-if-a-substring-exists-within-a-string-in-arduino#:~:text=In%20order%20to%20check%20if,searching%20for%20within%20another%20string.
+ * https://forum.arduino.cc/t/duvida-com-substring/1045145
+ * https://www.clubedohardware.com.br/forums/topic/1360900-como-utilizar-o-parseint-arduino/
+ * sudo chmod a+rw /dev/ttyUSB0
+
+ */
+
+
+int PINO_LED = 23;
+String recebido;
+int ledValue;
+int ldrMax;
+
+void setup() {
+  pinMode(PINO_LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Se receber algo pela serial
+  if (Serial.available() > 0) {
+    // Lê toda string recebida
+    recebido = leStringSerial();
+    // chama a funcao de processar string para comando no circuito
+    processCommand(recebido);
+  }
+}
+
+
+void processCommand(String command) {
+  // Verifica se o comando passado contém SET_LED
+  if (command.indexOf("SET_LED") >= 0) {
+    //caso seja verdadeiro, transforma o valor da string após o espaco
+    ledValue = command.substring(7, command.length()).toInt();
+    //verifica se esse valor esta no range de 0 a 100
+    if (ledValue >= 0 && ledValue <= 100 ) {
+      // valor maior que 0 ,  liga led
+      if (ledValue > 0 ) {
+        digitalWrite(PINO_LED, ledValue);
+        Serial.println("RES SET_LED 1");
+      } else {
+        // valor igual a 0 desliga o led
+        digitalWrite(PINO_LED, ledValue);
+        Serial.println("RES SET_LED 1");
+      }
+
+    } else
+      Serial.println("RES SET_LED-1");
+    // caso o range passado seja invalido
+  } else if (command.indexOf("GET_LED") >= 0)
+    //  retorna o valor do led atual
+    Serial.printf("RES GET_LED %d", ledGetValue());
+  else
+    Serial.println("ERR Unknown command.");
+
+}
+
+
+
+
+
+// Função para atualizar o valor do LED
+void ledUpdate() {
+  // Normalize o valor do LED antes de enviar para a porta correspondente
+}
+
+
+// Função para ler o valor do LDR
+int ldrGetValue() {
+  // Leia o sensor LDR e retorne o valor normalizado
+}
+
+
+
+// Função para ler o valor do LED
+int ledGetValue() {
+  return ledValue;
+}
+
+
+
+/**
+   Função que lê uma string da Serial
+*/
+
+String leStringSerial() {
+  String conteudo = "";
+  char caractere;
+  // Enquanto receber algo pela serial
+  while (Serial.available() > 0) {
+    // Lê byte da serial
+    caractere = Serial.read();
+    // Ignora caractere de quebra de linha
+    if (caractere != '\n') {
+      // Concatena valores
+      conteudo.concat(caractere);
+    }
+    // Aguarda buffer serial ler próximo caractere
+    delay(20);
+  }
+  return conteudo;
+}
