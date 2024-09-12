219ce8da293ee17b26808458870bb5fb155f04f3
Rafael Scalabrin <rafael.scalabrin@enablers.com.br>
2024-09-12 15:05:59 -0400

    ESP32: Teste das entradas e saídas analógicas.

--- smartlamp/smartlamp.ino
+++ smartlamp/smartlamp.ino
@@ -1,19 +1,28 @@
 // Defina os pinos de LED e LDR
 // Defina uma variável com valor máximo do LDR (4000)
 // Defina uma variável para guardar o valor atual do LED (10)
-int ledPin;
+int ledPin = 5;
 int ledValue;
 
-int ldrPin;
+int ldrPin = 4;
 // Faça testes no sensor ldr para encontrar o valor maximo e atribua a variável ldrMax
 int ldrMax;
+
+const int channel = 0;
+const int frequency = 5000;
+const int resolution = 12;
 
 void setup() {
     Serial.begin(9600);
     
     pinMode(ledPin, OUTPUT);
     pinMode(ldrPin, INPUT);
+
+      //set the resolution to 12 bits (0-4095)
+    analogReadResolution(resolution);
     
+    ledcAttach(ledPin, frequency, resolution);
+
     Serial.printf("SmartLamp Initialized.\n");
 
 
@@ -21,6 +30,19 @@ void setup() {
 
 // Função loop será executada infinitamente pelo ESP32
 void loop() {
+    ldrMax = analogRead(ldrPin);
+    // Serial.printf("Ldr Value = %d\r\n",ldrMax);
+    //analogWrite(ledPin, ldrMax);
+    ledcWrite(ledPin, ldrMax);
+    //digitalWrite(ledPin,1);
+
+    // for (int i = 0; i <= 4095; i++) { 
+    //   ledcWrite(ledPin, i);
+
+    //   delay(1);
+    // }
+
+    delay(10);
     //Obtenha os comandos enviados pela serial 
     //e processe-os com a função processCommand
 }
