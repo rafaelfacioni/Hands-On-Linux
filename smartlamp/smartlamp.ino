139779ebe8bb8f50c3d044a263452e7ec46e3d1f
Rafael Scalabrin <rafael.scalabrin@enablers.com.br>
2024-09-12 16:59:56 -0400

    ESP32: Implementação do firmware do ESP32 a partir das especificações da tarefa 1.

--- smartlamp/smartlamp.ino
+++ smartlamp/smartlamp.ino
@@ -1,65 +1,98 @@
 // Defina os pinos de LED e LDR
 // Defina uma variável com valor máximo do LDR (4000)
 // Defina uma variável para guardar o valor atual do LED (10)
-int ledPin = 5;
-int ledValue;
+#define LED_DEFAULT_INTENSITY 25
+#define ANALOG_RESOLTION 8
+#define LDR_MAX_INTENSITY 195
+#define LED_PIN 5
+#define LDR_PIN 4
 
-int ldrPin = 4;
-// Faça testes no sensor ldr para encontrar o valor maximo e atribua a variável ldrMax
-int ldrMax;
+int ledIntensity = 25;
 
-const int channel = 0;
-const int frequency = 5000;
-const int resolution = 8;
-
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     
-    pinMode(ledPin, OUTPUT);
-    pinMode(ldrPin, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(LDR_PIN, INPUT);
 
-      //set the resolution to 12 bits (0-4095)
-    analogReadResolution(resolution);
+    analogReadResolution(ANALOG_RESOLTION);//set the analog resolution
+    analogWriteResolution(LED_PIN,ANALOG_RESOLTION);
     
-    // ledcAttach(ledPin, frequency, resolution);
+    ledUpdate(LED_DEFAULT_INTENSITY);//Turns on LED with default intensity
 
     Serial.printf("SmartLamp Initialized.\n");
-
-
 }
 
 // Função loop será executada infinitamente pelo ESP32
-void loop() {
-    ldrMax = analogRead(ldrPin);
-    Serial.printf("Ldr Value = %d\r\n",ldrMax);
-    analogWrite(ledPin, ldrMax);
-    // ledcWrite(ledPin, ldrMax);
-    //digitalWrite(ledPin,1);
-
-    // for (int i = 0; i <= 4095; i++) { 
-    //   ledcWrite(ledPin, i);
-
-    //   delay(1);
-    // }
-
-    delay(10);
-    //Obtenha os comandos enviados pela serial 
-    //e processe-os com a função processCommand
+void loop()
+{
+  if (Serial.available())
+  {
+    String cmd = Serial.readStringUntil('\n'); // Read input command
+    cmd.trim(); // Remove any extra spaces or newlines
+    processCommand(cmd);
+  }
 }
 
 
-void processCommand(String command) {
-    // compare o comando com os comandos possíveis e execute a ação correspondente      
+void processCommand(String command)
+{
+  if (command.startsWith("SET_LED"))
+  {
+    int ledValue = command.substring(8).toInt(); // Extract intensity value
+    if (ledValue >= 0 && ledValue <= 100)
+    {
+      ledIntensity = map(ledValue, 0, 100, 0, 255); // Normalize to 0-255
+      ledUpdate(ledIntensity);
+      Serial.println("RES SET_LED 1");
+    }
+    else
+    {
+      Serial.println("RES SET_LED -1");
+    }
+  }
+  else if (command.equals("GET_LED"))
+  {
+    int ledValue = map(ledIntensity, 0, 255, 0, 100); // Convert back to 0-100 range
+    Serial.printf("RES GET_LED %d\r\n",ledValue);
+  }
+  else if (command.equals("GET_LDR"))
+  {
+    int ldrValue = ldrGetValue(); // Get LDR value
+    Serial.printf("RES GET_LDR %d\r\n",ldrValue);
+  }
+  else
+  {
+    Serial.printf("ERR Unknown command.\r\n");
+  }
 }
 
 // Função para atualizar o valor do LED
-void ledUpdate() {
+void ledUpdate(int intensity)
+{
+    analogWrite(LED_PIN, intensity); // Set LED intensity
     // Valor deve convertar o valor recebido pelo comando SET_LED para 0 e 255
     // Normalize o valor do LED antes de enviar para a porta correspondente
 }
 
 // Função para ler o valor do LDR
-int ldrGetValue() {
+int ldrGetValue()
+{
+  int ldrIntensity = analogRead(LDR_PIN);
+  int ldrPercentage = map(ldrIntensity, 0, LDR_MAX_INTENSITY, 0, 100);
+  if (ldrPercentage >= 0 && ldrPercentage <= 100)
+  {
+    return ldrPercentage;
+  }
+  else if(ldrPercentage<0)
+  {
+    return 0;
+  }
+  else
+  {
+    return 100;
+  }
     // Leia o sensor LDR e retorne o valor normalizado entre 0 e 100
     // faça testes para encontrar o valor maximo do ldr (exemplo: aponte a lanterna do celular para o sensor)       
     // Atribua o valor para a variável ldrMax e utilize esse valor para a normalização
